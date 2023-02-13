591b700f2aa7938dd3cc18ebaa51e8ef6ded753a
Andrei <musicmatters@mail.ru>
2023-02-13 09:34:51 +0600

    Печать элементов, соответствующих условию задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿Console.WriteLine("Введите элементы массива через пробел: ");
 string[] array = Console.ReadLine()!.Split(" ").ToArray();
+foreach (var element in array)
+{
+    if (element.Length <= 3)
+    {
+        Console.Write($"{element} ");
+    }
+    else
+    {
+        Console.Write("");
+    }
+}
